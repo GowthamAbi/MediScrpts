b7e5bd5c8fd5187355a779984bbb4923c8819104
“GowthamAbi” <gowtham2131ece@gmail.com>
2025-04-20 22:03:46 +0530

    grid applyin register

--- src/pages/Admin/Authentication/AdminRegister.jsx
+++ src/pages/Admin/Authentication/AdminRegister.jsx
@@ -18,9 +18,10 @@ const AdminRegister =()=>{
     ]
 
     return(
-        <div className="flex flex-col items-center h-auto space-y-4 justify-center max-w-7xl mx-auto mt-20 p-4 border rounded bg-slate-300">
+        <div className="bg-red-300 h-screen pt-20">
+        <div className="flex flex-col items-center h-auto space-y-4 justify-center max-w-5xl  mx-auto  p-4 border rounded bg-slate-300">
             <h1 className="text-xl mb-4 font-serif font-bold">Registeration</h1>
-            <form className=" flex flex-row flex-wrap gap-4 justify-center">
+            <form className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-center w-full">
             {regist.map((item,index)=>(
                 <input name={item.name} type={item.type} placeholder={item.placeholder} className="border p-2 rounded" />
             ))}   
@@ -32,7 +33,7 @@ const AdminRegister =()=>{
         </div>
 
         </div>
-            
+        </div>   
         
     )
 }
