6c2c2e40cccb9752d8ed4ab2010bfe7fe3ff2c92
“GowthamAbi” <gowtham2131ece@gmail.com>
2025-04-20 21:51:22 +0530

    modify register

--- src/pages/Admin/Authentication/AdminRegister.jsx
+++ src/pages/Admin/Authentication/AdminRegister.jsx
@@ -1,26 +1,36 @@
 const AdminRegister =()=>{
+    const regist=[
+        {name:'name' ,type:"text" ,placeholder:"Enter Name"  },
+        {name:'dob' ,type:"date" ,placeholder:"Enter DOB"  },
+        {name:'phone' ,type:"number" ,placeholder:"Enter Phone"  },
+        {name:'email' ,type:"email" ,placeholder:"Enter Email"  },
+        {name:'phone' ,type:"number" ,placeholder:"Enter Phone"  },
+        {name:'address' ,type:"text" ,placeholder:"Enter Address"  },
+        {name:'companyName' ,type:"text" ,placeholder:"Enter CompanyName"  },
+        {name:'companyPhone' ,type:"number" ,placeholder:"Enter CompanyPhone"  },
+        {name:'Companymail' ,type:"email" ,placeholder:"Enter CompanyEmail"  },
+        {name:'companyAddress' ,type:"text" ,placeholder:"Enter CompanyAddress"  },
+        {name:'gst' ,type:"text" ,placeholder:"Enter GST NO"  },
+        {name:'website' ,type:"text" ,placeholder:"Enter CompanyWebsite"  },
+        {name:'password' ,type:"text" ,placeholder:"Enter Password"  },
+        {name:'password' ,type:"text" ,placeholder:"Enter Conform Password"  }
+
+    ]
 
     return(
         <div className="flex flex-col items-center h-auto space-y-4 justify-center max-w-7xl mx-auto mt-20 p-4 border rounded bg-slate-300">
-            <h1 className="text-xl mb-4 font-serif font-bold text-xl">Registeration</h1>
+            <h1 className="text-xl mb-4 font-serif font-bold">Registeration</h1>
             <form className=" flex flex-row flex-wrap gap-4 justify-center">
-                <input name='name' type="text" placeholder="Enter Name" className="border p-2 rounded" />
-                <input name='dob' type="date" placeholder="Enter DOB" className="border p-2 rounded" />
-                <input name='phone' type="number" placeholder="Enter Phone" className="border p-2 rounded" />
-                <input name='email' type="email" placeholder="Enter Email" className="border p-2 rounded" />
-                <input name='phone' type="number" placeholder="Enter Phone" className="border p-2 rounded" />
-                <input name='address' type="text" placeholder="Enter Address" className="border p-2 rounded" />
-                <input name='companyName' type="text" placeholder="Enter CompanyName" className="border p-2 rounded" />
-                <input name='companyPhone' type="number" placeholder="Enter CompanyPhone" className="border p-2 rounded" />
-                <input name='Companymail' type="email" placeholder="Enter CompanyEmail" className="border p-2 rounded" />
-                <input name='companyAddress' type="text" placeholder="Enter CompanyAddress" className="border p-2 rounded" />
-                <input name='gst' type="text" placeholder="Enter GST NO" className="border p-2 rounded" />
-                <input name='website' type="text" placeholder="Enter CompanyWebsite" className="border p-2 rounded" />
-                <input name='password' type="text" placeholder="Enter Password" className="border p-2 rounded" />
-                <input name='password' type="text" placeholder="Enter Conform Password" className="border p-2 rounded" />    
+            {regist.map((item,index)=>(
+                <input name={item.name} type={item.type} placeholder={item.placeholder} className="border p-2 rounded" />
+            ))}   
                 
             </form>
             <button type="submit" className="bg-blue-600 px-8 py-4 rounded-xl font-serif text-xl font-bold text-white">Register</button>
+        <div>
+            
+        </div>
+
         </div>
             
         
